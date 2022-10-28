c0db9b11cfc63fe57e9641ee6cc8e14947a45dc4
Harihara sudhan <nhariharasudhan7@gmail.com>
2022-10-28 20:21:01 +0530

    Added token transfer function and helper function

--- contracts/Yebelo.sol
+++ contracts/Yebelo.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Yebelo is ERC20, Ownable {
-    uint[] Slab=[100,200,300,400,500]; // Slab along with index
+    uint[] Slab=[100,200,300,400,500];                  // Slab along with index
 
-    uint totalCapRemaining=Slab[4]+Slab[3]+Slab[2]+Slab[1]+Slab[0]; // Addition feature to check remaining capacity 
-    uint currentSlabcap=Slab[Slab.length-1]; // if slab array is empty no more insertion is allowed
-    //uint public deposited=0;  Keep tract of capacity
-    uint totalDeposited;
+    uint totalCapRemaining=Slab[4]+Slab[3]+Slab[2]+Slab[1]+Slab[0];                                 // Addition feature to check remaining capacity 
+    uint currentSlabcap=Slab[Slab.length-1];                            // if slab array is empty no more insertion is allowed
+    uint public deposited=0;                    //Keep track of capacity
+    //uint totalDeposited;  // For addition feature 
     uint lastTransfer;
 
     address payable own; // Name conflicts with Ownable contract's owner 
@@ -29,5 +29,36 @@ contract Yebelo is ERC20, Ownable {
         mint(msg.sender,3000);  // Minting 3000 tokens for contract caller so the address could transfer tokens
         own=payable(msg.sender);
 
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    /**
+     * Query for slab of a given address
+     */
+    function returnSlab(address _checker)public view returns (uint){
+        require(slabDetails[_checker].hasDeposited,"Deposit tokens before checking for slab."); // cannot check for slab of an address if the address has not deposited yet
+        return slabDetails[_checker].slab;
+    }
+
+    /**
+     * Core function to transfer tokens to contract address
+     */
+
+    function transferToContract(address _to,uint _amount)public payable {
+        
+        transfer(_to, _amount);
+        slabDetails[msg.sender].hasDeposited=true;
+        slabDetails[msg.sender].depositedAmount=slabDetails[msg.sender].depositedAmount+_amount;
+        deposited+=_amount;
+    }
+    /**
+     * Query for token balanceOf contract's address
+     */
+    function getBalanceOfToken(address _address) public view returns (uint) {
+        
+        return ERC20(_address).balanceOf(address(this));
     }
 }
